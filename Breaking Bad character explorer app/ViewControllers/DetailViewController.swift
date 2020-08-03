7d2eb95b00fd2824f643cc3faba906e4630d7967
Emil Vaklinov <emil_vaklinov@yahoo.com>
2020-08-03 13:00:04 +0300

    updates

--- Breaking Bad character explorer app/ViewControllers/DetailViewController.swift
+++ Breaking Bad character explorer app/ViewControllers/DetailViewController.swift
@@ -8,6 +8,23 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UITableViewController {
 
+    @IBOutlet weak var detailedTableView: UITableView!
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    override func numberOfSections(in tableView: UITableView) -> Int {
+
+        return 1
+    }
+    
+override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    // Dequeue the cell to display the user details
+    let cell = detailedTableView.dequeueReusableCell(withIdentifier: "detailedCell", for: indexPath)
+    return cell
+    }
 }
+
+
--- Breaking Bad character explorer app/ViewControllers/MasterViewController.swift
+++ Breaking Bad character explorer app/ViewControllers/MasterViewController.swift
@@ -12,29 +12,66 @@ class MasterViewController: UIViewController {
     let cellIdentifier = "baseCell"
     let apiManager = APIManager()
     var selectedIndexPath: IndexPath?
-   
+    
     @IBOutlet var searchBar: UISearchBar!
-  @IBOutlet var tableView: UITableView!
-   
-  override func viewDidLoad() {
-    super.viewDidLoad()
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+        var characters: [CharacterElement] = []
+    //    var charactersData: [CharactersData] = []
+    func search(_ text: String) {
+        
+        apiManager.searchFor(text) { [unowned self] outcome in
+            
+            switch outcome {
+            case .failure(let errorString):
+                print(errorString)
+                
+            case .success(let data):
+                
+                do {
+                    let result = try JSONParser.parse(data, type: CharacterRoot.self)
+                    self.characters = result.characters
+//                    self.saveInCoreDataWith(array: [])
+                    
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                        
+                    }
+                    
+                } catch {
+                    print("JSON Error: \(error)")
+                }
+                
+            }
+        }
+    }
+}
 
-}
-}
 
 extension MasterViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 3
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? MasterViewCell  else {
-            fatalError("The dequeued cell is not an instance of TableViewCell.")
-        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! MasterViewCell
     
         return cell
     }
     
+    
+}
+//extension BaseViewController: UITableViewDelegate {}
+
+extension MasterViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        search(searchBar.text ?? "")
+    }
     
 }
