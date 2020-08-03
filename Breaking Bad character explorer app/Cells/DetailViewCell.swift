85e25fab649bd9f1d658b76973d33ad9b68d344b
Emil Vaklinov <emil_vaklinov@yahoo.com>
2020-08-03 13:00:22 +0300

    detail cell

---
+++ Breaking Bad character explorer app/Cells/DetailViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewCell.swift
+//  Breaking Bad character explorer app
+//
+//  Created by Emil Vaklinov on 01/08/2020.
+//  Copyright © 2020 Emil Vaklinov. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewCell: UITableViewCell {
+
+    @IBOutlet weak var faceCharacter: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var status: UILabel!
+    @IBOutlet weak var nickName: UILabel!
+    @IBOutlet weak var occupation: UILabel!
+    @IBOutlet weak var seasonAppearance: UILabel!
+    
+}
