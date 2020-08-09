5125df90abe79e7f989446639e99d5350ff1adad
Emil Vaklinov <emil_vaklinov@yahoo.com>
2020-08-09 19:14:51 +0300

    tests init

---
+++ Breaking Bad character explorer appTests/Breaking_Bad_character_explorer_appTests.swift
@@ -0,0 +1,33 @@
+//
+//  Breaking_Bad_character_explorer_appTests.swift
+//  Breaking Bad character explorer appTests
+//
+//  Created by Emil Vaklinov on 09/08/2020.
+//  Copyright © 2020 Emil Vaklinov. All rights reserved.
+//
+
+import XCTest
+
+class Breaking_Bad_character_explorer_appTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
